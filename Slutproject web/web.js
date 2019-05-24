544bedfe1d0d77bb51839da452580d3e07b45e75
Casper Enarsson <45626406+Crossdonke@users.noreply.github.com>
2019-05-24 15:54:00 +0200

    Add files via upload

--- Slutproject web/web.js
+++ Slutproject web/web.js
@@ -23,3 +23,30 @@ if (fråga3 == "Textprogrammering") {
 document.getElementById("inskickatsvar").style.visibility="visible";
 document.getElementById("antalratt").innerHTML = "Du fick " + correct + " Rätt";
 }
+var countDownDate = new Date("May 29, 2019").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = "Nästa Tjejhack är om " + days + "dagar " + hours + "timmar "
+  + minutes + "minuter " + seconds + "sekunder ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
