7a16ea23ad250a7ed9d2294506ee4a00b49475c6
Casper Enarsson <45626406+Crossdonke@users.noreply.github.com>
2019-05-24 13:42:25 +0200

    Add files via upload

--- Slutproject web/web.js
+++ Slutproject web/web.js
@@ -1,3 +1,25 @@
 $('.carousel').carousel({
   interval: 3000
 })
+function kollasvar(){
+
+var fråga1 = document.quiz.fråga1.value;
+var fråga2 = document.quiz.fråga2.value;
+var fråga3 = document.quiz.fråga3.value;
+var correct= 0;
+
+if (fråga1 == "Monthy Python") {
+	correct++;
+}
+if (fråga2 == "Turtle") {
+	correct++;
+}
+if (fråga3 == "Textprogrammering") {
+	correct++;
+}
+
+
+
+document.getElementById("inskickatsvar").style.visibility="visible";
+document.getElementById("antalratt").innerHTML = "Du fick " + correct + " Rätt";
+}
